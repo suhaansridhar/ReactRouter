abc87bcdfe4be9a0a95ea26e3f9c4ba7c3b08728
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2023-05-04 21:14:36 +0100

    added components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import './App.css';
 import {Routes, Route, BrowserRouter, Link} from 'react-router-dom';
-import Home from './Home';
-import Books from './Books';
+import Home from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return (
-    <BrowserRouter>
-
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/Books">Books</Link></li>
-    </ul>
+    <div className="main--container">
+      <ul className="ordered--list--links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/About">About</Link></li>
+      </ul>
 
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/Books' element={<Books />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />}></Route>
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
 
